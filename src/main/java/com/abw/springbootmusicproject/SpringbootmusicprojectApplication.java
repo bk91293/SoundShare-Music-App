90ed83a9195e7be9b962387a0ed812496e4f7648
aleksandrawlazlo <aleksandra.wlazlo97@gmail.com>
2023-05-17 15:01:49 +0200

    first commit

---
+++ src/main/java/com/abw/springbootmusicproject/SpringbootmusicprojectApplication.java
@@ -0,0 +1,13 @@
+package com.abw.springbootmusicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmusicprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmusicprojectApplication.class, args);
+	}
+
+}
